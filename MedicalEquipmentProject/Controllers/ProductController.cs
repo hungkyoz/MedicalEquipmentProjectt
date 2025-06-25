75393a8bfc7ef9a938affc95b463c2cdc554ac7e
hungkyo <nguyencuuminhhung@gmail.com>
2025-06-25 16:49:07 +0700

    Hoàn thành tính năng Thêm sản phẩm bằng AJAX, nhiều ảnh WebP, validate và thông báo đẹp bằng SweetAlert2

--- MedicalEquipmentProject/Controllers/ProductController.cs
+++ MedicalEquipmentProject/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using MedicalEquipmentProject.Data;
+using MedicalEquipmentProject.Models;
+using MedicalEquipmentProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MedicalEquipmentProject.Controllers
 {
@@ -23,5 +26,60 @@ namespace MedicalEquipmentProject.Controllers
             var products = _context.Products.Include(p => p.ProductImages).ToList();
             return PartialView("_ProductListPartial", products);
         }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errorMessages = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage);
+
+                return BadRequest(new { errors = errorMessages });
+            }
+
+            var product = new Product
+            {
+                Name = model.Name,
+                Quantity = model.Quantity,
+                Date = model.Date,
+                Price = model.Price
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            if (model.Images != null && model.Images.Count > 0)
+            {
+                foreach (var image in model.Images)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                    var path = Path.Combine("wwwroot/product-images", fileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await image.CopyToAsync(stream);
+                    }
+
+                    _context.ProductImages.Add(new ProductImage
+                    {
+                        ProductId = product.Id,
+                        ImageUrl = "/product-images/" + fileName
+                    });
+                }
+
+                await _context.SaveChangesAsync();
+            }
+
+            var products = await _context.Products.Include(p => p.ProductImages).ToListAsync();
+            return PartialView("_ProductListPartial", products);
+        }
+
+
+
+
+
     }
 }
---
+++ MedicalEquipmentProject/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalEquipmentProject.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Required(ErrorMessage = "Tên sản phẩm là bắt buộc")]
+        public string Name { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Số lượng phải lớn hơn 0")]
+        public int Quantity { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Ngày nhập")]
+        [CustomValidation(typeof(ProductViewModel), nameof(ValidateDateNotInFuture))]
+        public DateTime Date { get; set; }
+
+        [Range(1, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0")]
+        public decimal Price { get; set; }
+
+        public List<IFormFile> Images { get; set; }
+
+        public static ValidationResult ValidateDateNotInFuture(DateTime date, ValidationContext context)
+        {
+            return date > DateTime.Today
+                ? new ValidationResult("Ngày không được vượt quá hôm nay")
+                : ValidationResult.Success;
+        }
+    }
+}
